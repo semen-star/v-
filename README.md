57d3a6d8edc8d13d4f562d273c83eebe41442bf3
Пользователь 6105 <user6105@vuz.magtu.ru>
2023-11-10 11:13:02 +0500

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+commands:
+
+строка               - string
+прыг_скок            - endl
+царь_батюшка_главный - int main
+внедрить             - using
+хутор                - namespace
+Русь                 - std
+молвить              - cout
+Внедрить_Руску_Молву - setlocale(LC_ALL, "Russian");
---
+++ V++/V++.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#define строка string
+#define прыг_скок endl
+#define царь_батюшка_главный int main
+#define внедрить using
+#define хутор namespace
+#define Русь std
+#define молвить cout
+#define Внедрить_Руску_Молву setlocale(LC_ALL, "Russian");
+
+внедрить хутор Русь;
+царь_батюшка_главный() {
+	Внедрить_Руску_Молву;
+	строка строченька = "Слався Русь!";
+	молвить << строченька << прыг_скок;
+}
