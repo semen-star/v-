61bbfa218b788c4180c50c845f918b99a0862e34
Konstantin Shemyakin <zdrastekostik@gmail.com>
2023-11-10 15:23:16 +0500

    Library update
    -added VS 2022 support with v143 build tools
    -added .gitignore file with .opendb extension because of an error with commit
    -added dictionary of old russian words(for more info open file "starorusskiye-slova.pdf")
    -all defines are now in the separate "V++.h" library file
    -added separation with regions, based on the type of defines(f.e. data types, operators, pointers, etc.)
    -added new defines:
    цифра_окоянная-int
    цифра_необименная-long long
    цифра_бесовская-float
    писмя-char
    лесть-bool
    ежели-if
    ежели_по_другому-else if
    ежели_не_так-else
    делать-do
    донежеле-while
    повторяй-for
    крабица-struct
    богоотец-class

--- V++/V++.cpp
+++ V++/V++.cpp
@@ -1,12 +1,5 @@
 ﻿#include <iostream>
-#define строка string
-#define прыг_скок endl
-#define царь_батюшка_главный int main
-#define внедрить using
-#define хутор namespace
-#define Русь std
-#define молвить cout
-#define Внедрить_Руску_Молву setlocale(LC_ALL, "Russian");
+#include "V++.h"
 
 внедрить хутор Русь;
 царь_батюшка_главный() {
