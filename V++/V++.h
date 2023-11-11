02b80a3474c641618672b9087d46dce5ef4f18f8
Semyon <ss0215292@gmail.com>
2023-11-11 09:08:50 +0500

    more words

--- V++/V++.h
+++ V++/V++.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+
 #pragma region Types
 
 // Даже самое большое дерево выросло из маленького семени.(типы данных)
@@ -10,12 +12,14 @@
 #define цифра_бесовская float
 #define писмя char
 #define лесть bool // также есть вариант "лжа"
+#define пустой void
 #pragma endregion
 
 #pragma region Main
 
 // Приветливое слово побеждает гнев.(основные операторы, функции)
-
+#define жили_были {
+#define конецъ }
 #define прыг_скок endl
 #define царь_батюшка_главный int main
 #define внедрить using
@@ -23,7 +27,7 @@
 #define Русь std
 #define молвить cout
 #define Внедрить_Руску_Молву setlocale(LC_ALL, "Russian");
-#define Внедрить_Руску_Ввод SetConsoleCP(1251);
+#define возздать return
 #pragma endregion
 
 #pragma region Constructions
@@ -42,5 +46,44 @@
 
 #pragma region Pointers and References 
 // Встречают по одежке, а провожают по уму(указатели)
-// WIP
+#define здесъ *
+#define исконный &
+#define послать_гонца_по *
+#pragma endregion
+
+#pragma region Types
+
+// Даже самое большое дерево выросло из маленького семени.(типы данных)
+
+#define строка string
+typedef const char* письмено;
+#define цифра_окоянная int
+#define цифра_необименная long long
+#define цифра_бесовская float
+#define писмя char
+#define лесть bool // также есть вариант "лжа"
+#pragma endregion
+
+#pragma region Miscellaneous
+// Небольшие былинки (разное)
+
+#define несокрушимый const
+#define переменчивый volatile
+#define беззначный unsigned
+#pragma endregion
+
+#pragma region Random
+// Куда стрела улетит (случайные значения)
+
+пустой эхЛихо() 
+жили_были
+	srand(time(NULL));
+конецъ
+
+
+цифра_окоянная кудаСтрелаПолетит(цифра_окоянная от, цифра_окоянная до) 
+жили_были
+	возздать rand() % (от - до) + от;
+конецъ
+
 #pragma endregion
